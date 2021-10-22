d663ad533a3ad40e7a92ec074f3d957b014be038
kawauzo <loic.fournerat@gmail.com>
2021-10-22 18:36:33 +0200

    +ft::pair, need more tests

---
+++ containers/utils/pair.hpp
@@ -0,0 +1,101 @@
+#ifndef PAIR_HPP
+# define PAIR_HPP
+
+namespace ft {
+
+template<
+    class T1,
+    class T2
+> struct pair {
+public:
+
+    /*
+     * **************************************
+     * ********** Member Types **************
+     * **************************************
+    */
+
+    typedef T1   first_type;
+    typedef T2   second_type;
+
+    /*
+     * **************************************
+     * ********* Member Objects *************
+     * **************************************
+    */
+
+    first_type  first;
+    second_type second;
+
+    /*
+     * **************************************
+     * ********* Member Functions ***********
+     * **************************************
+    */
+
+    // ***** Constructors *****
+
+    // default constructor, value initializes first and second
+    pair() : first(first_type()), second(second_type()) {}
+
+    // initializes first with x and second with y
+    pair( const T1& x, const T2& y ) : first(x), second(y) {}
+
+    // initializes first with p.first and second with p.second
+    template< class U1, class U2 >
+    pair( const pair<U1, U2>& p ) : first(p.first), second(p.second) {}
+
+    // ***** copy assignment operator *****
+    pair& operator=( const pair& other ) {
+        first = other.first;
+        second = other.second;
+        return *this;
+    }
+
+
+};
+
+/*
+ * ****************************************
+ * * Pair non-member relational operators *
+ * ****************************************
+*/
+template< class T1, class T2 >
+bool operator==( const ft::pair<T1,T2>& lhs, const ft::pair<T1,T2>& rhs ){
+    return (lhs.first == rhs.first && lhs.second == rhs.second) ? true : false;
+}
+
+template< class T1, class T2 >
+bool operator!=( const ft::pair<T1,T2>& lhs, const ft::pair<T1,T2>& rhs ){
+    return !(lhs == rhs);
+}
+
+template< class T1, class T2 >
+bool operator<( const ft::pair<T1,T2>& lhs, const ft::pair<T1,T2>& rhs ){
+    if (lhs.first < rhs.first)
+        return true;
+    if (rhs.first < lhs.first)
+        return false;
+    if (lhs.second < rhs.second)
+        return true;
+    return false;
+}
+
+template< class T1, class T2 >
+bool operator<=( const ft::pair<T1,T2>& lhs, const ft::pair<T1,T2>& rhs ){
+    return (lhs == rhs || lhs < rhs);
+}
+
+
+template< class T1, class T2 >
+bool operator>( const ft::pair<T1,T2>& lhs, const ft::pair<T1,T2>& rhs ){
+    return (lhs != rhs && !(lhs < rhs));
+}
+
+template< class T1, class T2 >
+bool operator>=( const ft::pair<T1,T2>& lhs, const ft::pair<T1,T2>& rhs ){
+    return (lhs == rhs || lhs > rhs);
+}
+
+}
+#endif
--- tests/map_tst.cpp
+++ tests/map_tst.cpp
@@ -1,5 +1,36 @@
 #include "tests.hpp"
 
+void cppr_pair(){
+    ft::pair<int,char> foo (10,'z');
+    ft::pair<int,char> bar (90,'a');
+
+    if (foo==bar) std::cout << "foo and bar are equal\n";
+    if (foo!=bar) std::cout << "foo and bar are not equal\n";
+    if (foo< bar) std::cout << "foo is less than bar\n";
+    if (foo> bar) std::cout << "foo is greater than bar\n";
+    if (foo<=bar) std::cout << "foo is less than or equal to bar\n";
+    if (foo>=bar) std::cout << "foo is greater than or equal to bar\n";
+
+    bar.first = 10;
+    std::cout << "\n";
+    if (foo==bar) std::cout << "foo and bar are equal\n";
+    if (foo!=bar) std::cout << "foo and bar are not equal\n";
+    if (foo< bar) std::cout << "foo is less than bar\n";
+    if (foo> bar) std::cout << "foo is greater than bar\n";
+    if (foo<=bar) std::cout << "foo is less than or equal to bar\n";
+    if (foo>=bar) std::cout << "foo is greater than or equal to bar\n";
+
+    bar.second = 'z';
+    std::cout << "\n";
+    if (foo==bar) std::cout << "foo and bar are equal\n";
+    if (foo!=bar) std::cout << "foo and bar are not equal\n";
+    if (foo< bar) std::cout << "foo is less than bar\n";
+    if (foo> bar) std::cout << "foo is greater than bar\n";
+    if (foo<=bar) std::cout << "foo is less than or equal to bar\n";
+    if (foo>=bar) std::cout << "foo is greater than or equal to bar\n";
+}
+
 void map_all_tests(){
 
+    cppr_pair();
 }
